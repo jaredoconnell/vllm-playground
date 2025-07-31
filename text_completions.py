ce1fa70193b752e5a0701dfc7b6831c75cf11d35
jaredoconnell <joconnel@redhat.com>
2025-07-31 21:33:10 +0000

    Progress towards getting script running

--- text_completions.py
+++ text_completions.py
@@ -1,7 +1,8 @@
 from typing import Optional, Union, Any, AsyncGenerator
-from vllm.engine.async_llm_engine import AsyncLLMEngine, AsyngEngineArgs, PromptType, SamplingParams, RequestOutput
+from vllm.engine.async_llm_engine import AsyncLLMEngine, AsyncEngineArgs, PromptType, SamplingParams, RequestOutput
 import time
 import os
+import asyncio
 
 model = "meta-llama/Llama-3.2-1B"
 output_token_count = 64
@@ -11,15 +12,14 @@ hf_token = os.environ.get("HF_TOKEN")
 # Check environment with:
 # python .venv/lib64/python3.9/site-packages/vllm/collect_env.py
 
-engine_args = AsyncEngineArgs(model=model, hf_token=hf_token, device="cuda")
+engine_args = AsyncEngineArgs(model=model, hf_token=hf_token)
 llm_engine = AsyncLLMEngine.from_engine_args(engine_args)
 
 async def handle_llm_output(
-    self,
     generator: AsyncGenerator[RequestOutput, None],
     start_time: float,
     request_id: Optional[str],
-) -> AsyncGenerator[None]: # AsyncGenerator[Union[StreamingTextResponse, ResponseSummary], None]:
+) -> None: # AsyncGenerator[Union[StreamingTextResponse, ResponseSummary], None]:
     response_prompt_count: Optional[int] = None
     response_output_count: Optional[int] = None
     iter_count = 0
@@ -43,7 +43,7 @@ async def handle_llm_output(
         iter_count += 1
         if output.finished:
             print("Got finished output:", output)
-            yield
+            return # yield
         else:
             print("Got unfinished output:", output)
             # yield StreamingTextResponse(
@@ -65,4 +65,4 @@ params = SamplingParams(
 request_id = "request_" + str(int(time.time()))
 start_time = time.time()
 llm_generator = llm_engine.generate("write a summary of the purpose of HTML", params, request_id)
-handle_llm_output(llm_generator, start_time, request_id)
+asyncio.run(handle_llm_output(llm_generator, start_time, request_id))
